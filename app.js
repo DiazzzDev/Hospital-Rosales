0e9d18e369d9197966bc4e69d327b48837776f62
DiazzzDev <ed.diaz.hz@gmail.com>
2026-06-18 08:19:50 -0600

    feat: files created

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import logoutRoutes from "./routes/logout.js"
+import logoutRoutes from "./src/routes/logout.js"
 
 const app = express()
 
